ceb9d9300bf00afbe5635f0370bf93703b479c7f
Vedant Kokane <120434854+Vedant-kokane-PICT@users.noreply.github.com>
2024-11-03 13:10:34 +0530

    Update Bank.sol

--- 03_Blockchain_Technology/Bank.sol
+++ 03_Blockchain_Technology/Bank.sol
@@ -2,32 +2,27 @@
 pragma solidity ^0.8.0;
 
 contract Bank{
-     // Mapping to store the balance of each account holder (address)
-    mapping(address => uint256) private balances;
+    address public owner;
+    uint balance;
 
-    // Event to log deposits
-    event Deposit(address indexed account, uint256 amount);
 
-    // Event to log withdrawals
-    event Withdraw(address indexed account, uint256 amount);
+    function deposit(uint amount) public {
+        if(amount < 0){
+            revert("Amount should be positive");
+        }
+        balance += amount;
 
-    // Deposit function to add money to the sender's account
-    function deposit() public payable {
-        require(msg.value > 0, "Deposit amount must be greater than zero.");
-        balances[msg.sender] += msg.value;
-        emit Deposit(msg.sender, msg.value);
     }
 
-    // Withdraw function to withdraw money from the sender's account
-    function withdraw(uint256 withdrawAmount) public {
-        require(balances[msg.sender] >= withdrawAmount, "Insufficient balance.");
-        balances[msg.sender] -= withdrawAmount;
-        payable(msg.sender).transfer(withdrawAmount);
-        emit Withdraw(msg.sender, withdrawAmount);
+    function withdraw(uint amount) public{
+        if(balance < amount){
+            revert("Cannot withdraw money as balance is lower than amount to be withdrawn");
+        }
+        balance -= amount;
     }
+    
 
-    // Function to check the balance of the sender's account
-    function checkBalance() public view returns (uint256) {
-        return balances[msg.sender];
+    function showBalance() public view returns (uint){
+        return balance;
     }
 }
