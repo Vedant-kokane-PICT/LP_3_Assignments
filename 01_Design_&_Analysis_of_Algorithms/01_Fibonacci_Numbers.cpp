d8a185672a7c2e762f3ac98c0993374fd4831d36
Vedant Kokane <120434854+Vedant-kokane-PICT@users.noreply.github.com>
2024-11-06 10:15:37 +0530

    Update 01_Fibonacci_Numbers.cpp

--- 01_Design_&_Analysis_of_Algorithms/01_Fibonacci_Numbers.cpp
+++ 01_Design_&_Analysis_of_Algorithms/01_Fibonacci_Numbers.cpp
@@ -1,31 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-// Using recursive approach
 // TC - O(2^N) , SC - O(N) (stack space)
+// Recursive Approach
 int helper1(int n){
     if(n <= 1){
         return n;
     }
     return helper1(n-1) + helper1(n-2);
-} 
+}
 
-// using iterative approach
+
 //  TC - O(N) , SC - O(1) 
+// Iterative Approachx
 int helper2(int n){
     int first = 0;
     int second = 1;
     for(int i=2;i<=n;i++){
-      int third = first + second;
-      first = second;
-      second = third;
+        int third = first + second;
+        first = second;
+        second = third;
     }
     return second;
-} 
+}
 
-int main(){
-  cout<<helper1(8);
-  cout<<endl;
-  cout<<helper2(8);
-  return 0;
+int main()
+{
+    int n = 10;
+    cout<<helper1(n)<<endl;
+    cout<<helper2(n)<<endl;
+    return 0;
 }
